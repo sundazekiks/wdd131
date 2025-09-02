4b60b346be413edda282d8673dbe99a7cf9b898c
sundazekiks <abutontravis1@gmail.com>
2025-09-02 14:33:14 -0400

    first changes

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const time = new Date();
+
+document.getElementById('currentyear').innerHTML = `${time.getFullYear()}`;
+
+document.getElementById('lastModified').innerHTML = `${document.lastModified}`;
